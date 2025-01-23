fed35356362d00899e07540075c2958e5de0418f
alan-campos-4 <alvaroespi2003@gmail.com>
2025-01-23 12:19:04 +0100

    manifest modified

--- __manifest__.py
+++ __manifest__.py
@@ -8,8 +8,8 @@
 Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Alvaro Espinosa",
+    'website': "https://www.modulo_marisma.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
